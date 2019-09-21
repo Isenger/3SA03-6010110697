d572890db6687a0cc8c423c8fddcfc50bc49112e
Isenger <6010110697@psu.ac.th>
2019-09-22 02:53:57 +0700

    test

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './App.css';
+import WordCard from './WordCard';
+
+
+const word = ['Heineken','Federbrau','Singha']
+var item = word[Math.floor(Math.random()*word.length)];
+
+// const word = "Hello";
+class App extends Component {
+
+  newgame = () => {
+    window.location.reload(false);
+  }
+  render() {
+    return (
+      <div className= "App">
+        <h1 className="welcom"> Let To Play Game</h1>
+
+        <WordCard value={item.toUpperCase()}/>
+        <h1 id="result"></h1>
+        <button id="newgame" className="button" onClick={this.newgame}>New Game</button>
+      </div>
+    );
+
+  }
+}
+export default App;
+
+
+
+
